6123d97ca7ef82e6a7156d9480b3b463b0482652
fabricio-milanio <milanio.fabricio@gmail.com>
2024-01-31 22:17:54 -0300

    :sparkles: create footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { FooterContainer } from './footer';
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <footer>
+        <p>
+          Desenvolvido por Fabrício Milanio
+          <i className="ph-thin ph-trademark-registered"></i>
+        </p>
+      </footer>
+    </FooterContainer>
+  );
+}
---
+++ src/components/footer/footer.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 10vh;
+  background-color: black;
+  footer {
+    display: flex;
+    align-items: center;
+    p {
+      color: white;
+      font-size: 1.5rem;
+    }
+  }
+`;
