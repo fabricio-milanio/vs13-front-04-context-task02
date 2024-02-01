a8a33d82e5a8d1d106d728179a883b2d004f4c23
fabricio-milanio <milanio.fabricio@gmail.com>
2024-01-31 22:19:06 -0300

    :lipstick: create stylization

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: lightblue;
+  max-height: 100vh;
+} */
+`;
---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  max-height: 100vh;
+  height: 100%;
+`;
